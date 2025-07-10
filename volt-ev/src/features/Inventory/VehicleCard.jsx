6657059c513b84c40cc4ceff7c868a52b95ea49f
lucastefanescu <lucastefanescu383@gmail.com>
2025-07-09 22:13:38 -0400

    new additions

--- volt-ev/src/features/Inventory/VehicleCard.jsx
+++ volt-ev/src/features/Inventory/VehicleCard.jsx
@@ -1,5 +1,24 @@
+import blackCarImage from "../../Media/EV Olive View.png";
+
 function VehicleCard() {
-    return ();
+	return (
+		<div>
+			<img src={blackCarImage} />
+			<h1>Model R</h1>
+			<div>
+				<h2>2023</h2>
+			</div>
+			<div>
+				<img />
+				<h3></h3>
+				<img />
+				<h3></h3>
+				<img />
+			</div>
+			<h2></h2>
+			<button></button>
+		</div>
+	);
 }
 
 export default VehicleCard;
